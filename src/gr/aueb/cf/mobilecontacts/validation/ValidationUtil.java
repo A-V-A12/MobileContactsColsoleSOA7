fe955e5f290a750a256aa047c8ac16cebd5cb617
A-V-A12 <a-v-a.12l@protonmail.com>
2025-05-16 20:34:53 +0300

    Validator created

---
+++ src/gr/aueb/cf/mobilecontacts/validation/ValidationUtil.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.mobilecontacts.validation;
+
+import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+
+public class ValidationUtil {
+    /**
+     * No instances of this class should be available.
+     */
+    private ValidationUtil(){
+
+    }
+
+    public static String validateDTO(MobileContactInsertDTO insertDTO){
+        String errorResponse = "";
+
+        if (insertDTO.getPhoneNumber().length() <= 5)
+            errorResponse += "Ο Τηλεφωνικός Αριθμός πρέπει να έχει περισσότερα από πέντε σύμβολα.\n";
+        if (insertDTO.getPhoneNumber().length() < 2)
+            errorResponse += "Ο Τηλεφωνικός Αριθμός πρέπει να περιέχει δύο ή περισσότερους χαρακτήρες.\n";
+        if (insertDTO.getLastname().length() < 2)
+            errorResponse += "Το Επώνυμο πρέπει να περιέχει δύο ή περισσότερους χαρακτήρες.\n";
+
+        return errorResponse;
+    }
+
+    public static String validateDTO(MobileContactInsertDTO updateDTO){
+        String errorResponse = "";
+
+        if (updateDTO.getPhoneNumber().length() <= 5)
+            errorResponse += "Ο Τηλεφωνικός Αριθμός πρέπει να έχει περισσότερα από πέντε σύμβολα.\n";
+        if (updateDTO.getPhoneNumber().length() < 2)
+            errorResponse += "Ο Τηλεφωνικός Αριθμός πρέπει να περιέχει δύο ή περισσότερους χαρακτήρες.\n";
+        if (updateDTO.getLastname().length() < 2)
+            errorResponse += "Το Επώνυμο πρέπει να περιέχει δύο ή περισσότερους χαρακτήρες.\n";
+
+        return errorResponse;
+    }
+}
